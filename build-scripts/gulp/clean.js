0a31388bd98375c2c150eca5ff7336f2e5cceb0f
Gertjan <gertjan.stulp@friss.com>
2022-08-16 13:16:13 +0200

    Fixed issue with gulp clean task deleting too many files

--- build-scripts/gulp/clean.js
+++ build-scripts/gulp/clean.js
@@ -3,9 +3,6 @@ const del = require("del");
 
 gulp.task("clean", (task) => {
     del.sync([
-        "./homeassistant-frontend/build/**", 
-        "./homeassistant-frontend/build"]);
-    del.sync([
         "./react_frontend/**/*.js", 
         "./react_frontend/**/*.json", 
         "./react_frontend/**/*.gz", 
